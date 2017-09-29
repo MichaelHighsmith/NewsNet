eedf672c4d2f11d29db86c82c68b13681eb6e9db
Michael Highsmith <mhighsm1@slu.edu>
2017-09-29 11:54:07 -0500

    urls, images, sharedpref source

--- app/src/main/java/com/satyrlabs/newsnet/ui/MainActivity.java
+++ app/src/main/java/com/satyrlabs/newsnet/ui/MainActivity.java
@@ -1,5 +1,8 @@
 package com.satyrlabs.newsnet.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,9 +27,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements SourceAdapter.AdapterCallback{
+public class MainActivity extends AppCompatActivity implements SourceAdapter.AdapterCallback, NewsAdapter.ArticleCallback{
 
-    //API key 0b86b2841499436cb769406b08f0ad62
     //powered by "NewsAPI.org"
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -34,9 +36,7 @@ public class MainActivity extends AppCompatActivity implements SourceAdapter.Ada
 
     RecyclerView recyclerView;
     RecyclerView sourceRecyclerView;
-
     NewsInterface apiService;
-
     SourceAdapter sourceAdapter;
 
     @Override
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity implements SourceAdapter.Ada
 
     //Set the source recyclerview(and call the main newsfeed method)
     public void initialCall(){
-
         Call<SourcesResponse> sourceCall = apiService.getNewsSources("en", API_KEY);
         sourceCall.enqueue(new Callback<SourcesResponse>() {
             @Override
@@ -81,35 +80,11 @@ public class MainActivity extends AppCompatActivity implements SourceAdapter.Ada
                 Log.e(TAG, t.toString());
             }
         });
-        callNewsFeed();
 
-    }
-
-    //Set the main news feed to the selected news source
-    public void callNewsFeed(){
-        //Call the getLatestNews method from interface and save the returned value as a call
-        Call<NewsResponse> call = apiService.getLatestNews("techcrunch", API_KEY);
-        //enqueue the call (asynchronous)
-        call.enqueue(new Callback<NewsResponse>() {
-            @Override
-            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
-                int statusCode = response.code();
-                //Check that the response is not null
-                if(response.body() != null){
-                    //get the list of articles (each one matches the POJO 'News')
-                    List<News> news = response.body().getArticles();
-                    recyclerView.setAdapter(new NewsAdapter(news, R.layout.list_item_news, getApplicationContext()));
-                } else {
-                    Log.v(response.toString(), "is null");
-                }
-                Log.v(Integer.toString(statusCode), "is the status code");
-            }
-
-            @Override
-            public void onFailure(Call<NewsResponse> call, Throwable t) {
-                Log.e(TAG, t.toString());
-            }
-        });
+        //Check for what the most recent source was and then open it first
+        SharedPreferences sharedPreferences = getSharedPreferences("myPref", 0);
+        String currentSource = sharedPreferences.getString("source", "techcrunch");
+        callNewsFeed(currentSource);
     }
 
     //Called when a new source is clicked on
@@ -125,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements SourceAdapter.Ada
                 if(response.body() != null){
                     //get the list of articles (each one matches the POJO 'News')
                     List<News> news = response.body().getArticles();
-                    recyclerView.setAdapter(new NewsAdapter(news, R.layout.list_item_news, getApplicationContext()));
+                    recyclerView.setAdapter(new NewsAdapter(news, R.layout.list_item_news, getApplicationContext(), MainActivity.this));
                 } else {
                     Log.v(response.toString(), "is null");
                 }
@@ -142,7 +117,22 @@ public class MainActivity extends AppCompatActivity implements SourceAdapter.Ada
 
     @Override
     public void onItemClicked(String id) {
+        //load the new sources articles
         callNewsFeed(id);
+        //update the sharedpref
+        SharedPreferences sharedPreferences = getSharedPreferences("myPref", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("source", id);
+        editor.apply();
     }
 
+    @Override
+    public void onArticleClicked(String url) {
+        //Launch an intent to the articles url on its homepage
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+
+
+    }
 }
--- app/src/main/java/com/satyrlabs/newsnet/ui/NewsAdapter.java
+++ app/src/main/java/com/satyrlabs/newsnet/ui/NewsAdapter.java
@@ -1,16 +1,23 @@
 package com.satyrlabs.newsnet.ui;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.satyrlabs.newsnet.R;
 import com.satyrlabs.newsnet.pojo.News;
+import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 
@@ -20,23 +27,36 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
     private int rowLayout;
     private Context context;
 
+    ArticleCallback callback;
+
+    public interface ArticleCallback{
+        void onArticleClicked(String url);
+    }
+
     public static class NewsViewHolder extends RecyclerView.ViewHolder{
         LinearLayout newsLayout;
+        LinearLayout titleLayout;
+        LinearLayout imageLayout;
         TextView articleTitle;
         TextView articleDescription;
+        ImageView articleImage;
 
         public NewsViewHolder(View v){
             super(v);
             newsLayout = (LinearLayout) v.findViewById(R.id.news_layout);
+            titleLayout = (LinearLayout) v.findViewById(R.id.title_layout);
+            imageLayout = (LinearLayout) v.findViewById(R.id.image_layout);
             articleTitle = (TextView) v.findViewById(R.id.title);
             articleDescription = (TextView) v.findViewById(R.id.description);
+            articleImage = (ImageView) v.findViewById(R.id.image);
         }
     }
 
-    public NewsAdapter(List<News> news, int rowLayout, Context context) {
+    public NewsAdapter(List<News> news, int rowLayout, Context context, ArticleCallback callback) {
         this.news = news;
         this.rowLayout = rowLayout;
         this.context = context;
+        this.callback = callback;
     }
 
     @Override
@@ -48,8 +68,24 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
 
     @Override
     public void onBindViewHolder(NewsViewHolder holder, final int position){
+        //set textviews with data from response
         holder.articleTitle.setText(news.get(position).getTitle());
         holder.articleDescription.setText(news.get(position).getDescription());
+        //Use Picasso and the image url to load image on the news feed
+        Picasso.with(context).load(news.get(position).getUrlToImage()).into(holder.articleImage);
+
+        //implement the articlecallback interface for clicks (sends to url in MainActivity)
+        final String url = news.get(position).getUrl();
+        holder.newsLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(callback!=null){
+                    callback.onArticleClicked(url);
+                }else{
+                    Log.v("error in calling url", url);
+                }
+            }
+        });
     }
 
     @Override
